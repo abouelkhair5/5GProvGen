a3ccc575cfede2f20525fd80f4d4f364c9663b57
Amr Abouelkhair <abouelkhair.305@gmail.com>
2024-08-27 13:12:16 -0400

    Add benign activity

---
+++ benign_activity_generator.py
@@ -0,0 +1,14 @@
+import subprocess
+
+class BenignActivity:
+    def __init__(self):
+        pass
+    
+    def start(self):
+        enable_activity = []
+        result = subprocess.run(enable_activity)
+    
+    def stop(self):
+        disable_activity = []
+        result = subprocess.run(disable_activity)
+        
