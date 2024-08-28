2244ba5ebc965fb571de6ffa13ee95e339fe8ad7
Amr Abouelkhair <abouelkhair.305@gmail.com>
2024-08-28 15:07:16 -0400

    Update benign activity with kube library

--- benign_activity_generator.py
+++ benign_activity_generator.py
@@ -1,14 +1,16 @@
 import subprocess
+from kubernetes import client, config
+import yaml
 
 class BenignActivity:
     def __init__(self):
-        pass
-    
+        self.config_file = yaml.safe_load("open5gs-k8s/open5gs/kustomization.yaml") 
+        config.load_kube_config()
+        self.apps_api = client.AppsV1Api()
+     
     def start(self):
-        enable_activity = []
-        result = subprocess.run(enable_activity)
+        self.apps_api.create_namespaced_deployment(self.config_file, namespace='open5gs')
+        
     
     def stop(self):
-        disable_activity = []
-        result = subprocess.run(disable_activity)
-        
+        self.apps_api.delete_namespaced_deployment(self.config_file, namespace='open5gs')
